1e01209bad41ec3058812b4d0d647061b07b3caa
Felipe Souza <feliperbs06@gmail.com>
2024-08-08 09:16:28 -0300

    password change and reset

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,10 +25,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "crispy_forms",
     "crispy_bootstrap5",
-
     "accounts",
     "pages",
 ]
@@ -123,3 +121,5 @@ LOGOUT_REDIRECT_URL = "home"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
