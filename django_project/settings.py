888c0229f53fb315481b1c688c170705ad068a3e
Felipe Souza <feliperbs06@gmail.com>
2024-08-08 16:17:29 -0300

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     "accounts",
     "pages",
+    "articles",
 ]
 
 MIDDLEWARE = [
@@ -97,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
